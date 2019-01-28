5feda7adca9faf62f2405245b88fde4bb0620767
denistornado <skorokhod1997@gmail.com>
2019-01-29 01:32:52 +0300

    beta without video

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,9 @@
 // Your code here...
+jQuery(document).ready(function($){
+  
+  $(".fa-search").click(function(){
+    $(".wrap, .input").toggleClass("active");
+    $("input[type='text']").focus();
+  });
+  
+});
