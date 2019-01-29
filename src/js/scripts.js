8dfcc4098f8f16ce6f8679b04c6b2314e6f31ba8
denistornado <skorokhod1997@gmail.com>
2019-01-30 01:27:45 +0300

    beta slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,25 @@
 // Your code here...
-jQuery(document).ready(function($){
-  
-  $(".fa-search").click(function(){
-    $(".wrap, .input").toggleClass("active");
-    $("input[type='text']").focus();
+$(document).ready(function(){
+  $('.your-class').slick({
+    
+    prevArrow:$('.main_step-by-step__button-prev'),
+    nextArrow:$('.main_step-by-step__button-next'),
+    slidesToShow: 3,
+    speed: 300,
+    centerPadding:50,
   });
+  $(".fa-search").click(function(){
+
+    $(".wrap, .input").toggleClass("active");
+
+    $("input[type='text']").focus();
+
+  });
+
   
+
 });
+
+
+  
+
